613fe4be25f361fa2c5a4ef99d4bdb7216f52e66
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-05-17 16:19:59 +0800

    wew

--- app/Http/Controllers/Api/ChirpstackController.php
+++ app/Http/Controllers/Api/ChirpstackController.php
@@ -18,17 +18,18 @@ class ChirpstackController extends Controller
         $obj = json_decode($json);
         $decoded = json_encode($obj->objectJSON);
         $a = \json_decode($decoded);
+        $aa = \json_decode($a);
 
         $date = new \DateTime;
         $date->modify('-1 minutes');
         $formatted_date = $date->format('Y-m-d H:i:s');
-        $device = DeviceData::where('code',$a->uniqueid)->where('created_at','>=', $formatted_date)->count();
+        $device = DeviceData::where('code',$aa->uniqueid)->where('created_at','>=', $formatted_date)->count();
         
         if($device == 0){
             $wew = new DeviceData;
-            $wew->coordinates = json_encode($a->gps);
-            $wew->status = $a->status;
-            $wew->code = $a->uniqueid;
+            $wew->coordinates = json_encode($aa->gps);
+            $wew->status = $aa->status;
+            $wew->code = $aa->uniqueid;
             $wew->save();
 
             broadcast(new AssetLocation($wew));
